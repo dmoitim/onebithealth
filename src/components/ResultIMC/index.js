43427533635d38be6b2fa1402d4291840c1b8664
Devair Luiz Moitim <dmoitim@gmail.com>
2023-09-27 20:09:45 -0300

    Aula 3 - Parte 3

--- src/components/ResultIMC/index.js
+++ src/components/ResultIMC/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
+import styles from "./style";
 
 export default function ResultIMC(props) {
     return (
-        <View>
-            <Text>{props.messageResultIMC} {props.resultIMC}</Text>
+        <View style={styles.resultImc}>
+            <Text style={styles.information}>{props.messageResultIMC} {props.resultIMC}</Text>
         </View>
     );
 }
---
+++ src/components/ResultIMC/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    resultImc: {
+        flex: 1,
+        marginTop: 15,
+        borderRadius: 50,
+        alignItems: 'center',
+        width: '100%',
+    },
+    information: {
+        fontSize: 18,
+        color: '#ff0043',
+        fontWeight: 'bold',
+    },
+});
+
+export default styles;
